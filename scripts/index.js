db973e215bb85c89b058064e62b20494eb56e57e
CodeSandbox <support@codesandbox.io>
2023-06-10 14:01:35 +0000

    Initial commit

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+import "/styles/main.scss";
+
+window.toggleNav = function (event) {
+  // renamed function to reflect updated behavior
+  event.stopPropagation();
+  var sidebarIsOpen =
+    document.getElementById("mySidepanel").style.width === "250px";
+
+  if (sidebarIsOpen) {
+    document.getElementById("mySidepanel").style.width = "0";
+  } else {
+    document.getElementById("mySidepanel").style.width = "250px";
+  }
+};
+
+document.addEventListener("click", function (event) {
+  var isClickInside = document
+    .getElementById("mySidepanel")
+    .contains(event.target);
+  var sidebarIsOpen =
+    document.getElementById("mySidepanel").style.width === "250px";
+  if (!isClickInside && sidebarIsOpen) {
+    window.toggleNav(event); // updated to use toggleNav function
+    console.log("toggle sidebar");
+  }
+});
